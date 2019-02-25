1c25285d93df7dd4a1826319f120cc79c7379850
iddinev <>
2019-02-25 09:31:21 +0200

    Main functions almost completed.

--- rsync_backup_and_targz.sh
+++ rsync_backup_and_targz.sh
@@ -1,93 +1,181 @@
-#!/usr/bin/env bash
+#!/USR/BIN/ENV BASH
 
 
 BACKUP_SCRIPT="/mnt/backup/code/rsync_backup.sh"
 TIMESTAMP_FILE="BACKUP-TIMESTAMP"
 BACKUP_TARGZ_DIR="/mnt/backup/backup_targz/"
-REMOTE_BACKUP_DIR="/mnt/storage/backups/pc_backup/"
 BACKUP_TARGZ_PREFIX="backup-"
 BACKUP_TARGZ_SUFFIX=".tar.gz"
+BACKUP_GPG_SUFFIX=".crypt"
 SSH_USER="root"
 SSH_HOST="mypi"
+REMOTE_BACKUP_DIR="/mnt/storage/backups/pc_backup/"
+# Should be the same as the max_num of backups in the backup script,
+# otherwise you duplicate backups.
+MAX_TARGZ=1
 
 
 function create_rsync_backup()
 {
-	rc_code=1
+	l_ret_code=1
 	if "$BACKUP_SCRIPT" --backup; then
-		rc_code=0
+		l_ret_code=0
 		log_systemd "Backup created."
 	fi
-	return "$rc_code"
+	return "$l_ret_code"
 }
 
-function get_latest_rsync_backup()
+function _count_backup()
 {
-	rc_val=-1
-	mapfile -t backup_list < <("$BACKUP_SCRIPT" --list 2>/dev/null)
-	if [ "${#backup_list[@]}" -ge 2 ]; then
-		latest_backup="$(echo ${backup_list[1]} | cut -d ' ' -f 2)"
-		ret_val="$latest_backup"
+	l_ret_val=-1
+	l_list_cmd="${1:-None}"
+	if [ "$l_list_cmd" != "None" ]; then
+		l_num_backup=$($l_list_cmd 2>/dev/null | wc -l)
+		if [ -n "$l_num_backup" ]; then
+			l_ret_val="$l_num_backup"
+		fi
 	fi
-	echo "$ret_val"
+
+	echo "$l_ret_val"
 }
 
-function targz_and_scp_backup()
+function count_targz_backup()
 {
-	rc_code=1
-	latest_backup="$(get_latest_rsync_backup)"
-	echo "Found new backup $latest_backup ." | systemd -t backup -p info
-	if [ "$latest_backup" != "-1" ] && [ "$latest_backup" != "" ] ; then
-		read timestamp < "$latest_backup"/"$TIMESTAMP_FILE"
-		targz_file="${BACKUP_TARGZ_DIR}/backup-${timestamp}.tar.gz"
-		if tar -zcf "$targz_file" "$latest_backup"; then
-			echo "Created $targz_file ." | systemd-cat -t backup -p info
-			# Overwrite previous backup.
-			if scp "$targz_file" "$SSH_USER"@"$SSH_USER":"$REMOTE_BACKUP_PATH"; then
-				echo "scp successfull." | systemd -t backup -p info
-				rc_code=0
+	l_ret_val=-1
+
+	l_cmd="ls -1dq ${BACKUP_TARGZ_DIR}*"
+	l_result="$(_count_backup $l_cmd)"
+	if [ "$l_result" != "-1" ]; then
+		l_ret_val="$l_result"
+	fi
+
+	echo "$l_ret_val"
+}
+
+function count_scp_backup()
+{
+	l_ret_val=-1
+
+	l_cmd="ssh ${SSH_USER}@${SSH_HOST} 'ls -1dq ${REMOTE_BACKUP_PATH}*'"
+	l_result="$(_count_backup $l_cmd)"
+	if [ "$l_result" != "-1" ]; then
+		l_ret_val="$l_result"
+	fi
+
+	echo "$l_ret_val"
+}
+
+function _rotate_backup()
+{
+	l_ret_code=1
+
+	l_count_cmd="${1:-None}"
+	l_list_cmd="${2:-None}"
+	l_rm_cmd="${3:-None}"
+
+	if [ "$l_count_cmd" != "None" ] && [ "$l_list_cmd" != "None" ]\
+	&& [ "$l_rm_cmd" ]; then
+		l_num_backup="$($l_count_cmd)"
+		l_rc=$?
+
+		if [ "$l_rc" -eq 0 ] && [ "$l_num_backup" -ge 0 ]; then
+			if [ "$l_num_backup" -gt "$MAX_TARGZ" ]; then
+				mapfile -t l_backup_list < <($l_list_cmd 2>/dev/null)
+				for i in $(eval "echo {$MAX_TARGZ..$l_num_backup}"); do
+					log_systemd "Rotating away ${l_backup_list[i]} ."
+					"$l_rm_cmd ${l_backup_list[i]}"
+				done
+				l_ret_code=0
+			else
+				l_ret_code=0
 			fi
 		fi
 	fi
 
-	return "$rc_code"
+	return "$l_ret_code"
 }
 
-function gpg_encrypt()
+function rotate_targz()
 {
-	true
+	l_ret_code=1
+
+	l_num_targz="$(count_targz_backup)"
+	l_rc=$?
+
+	if [ "$l_rc" -eq 0 ] && [ "$l_num_targz" -ge 0 ]; then
+		if [ "$l_num_targz" -gt "$MAX_TARGZ" ]; then
+			mapfile -t l_backup_list < <(ls -dqt "$BACKUP_TARGZ_DIR"* 2>/dev/null)
+			for i in $(eval "echo {$MAX_TARGZ..$l_num_targz}"); do
+				log_systemd "Rotating away ${l_backup_list[i]} ."
+				rm -rf "${l_backup_list[i]}"
+			done
+			l_ret_code=0
+		else
+			l_ret_code=0
+		fi
+	fi
+
+	return "$l_ret_code"
+}
+
+function get_latest_rsync_backup()
+{
+	l_ret_val=-1
+	mapfile -t l_backup_list < <("$BACKUP_SCRIPT" --list 2>/dev/null)
+	if [ "${#l_backup_list[@]}" -ge 2 ]; then
+		l_latest_backup="$(echo ${l_backup_list[1]} | cut -d ' ' -f 2)"
+		l_ret_val="$l_latest_backup"
+	fi
+	echo "$l_ret_val"
+}
+
+function gpg2_encrypt()
+{
+	l_ret_code=1
+	mapfile -t l_backup_list < <(ls -dqtr "$BACKUP_TARGZ_DIR"* 2>/dev/null)
+	if [ "${#l_backup_list[@]}" -ge 1 ]; then
+		l_backup_file="${l_backup_list[0]}"
+		if gpg2 --batch -c --cipher-algo AES256\ 
+		--passphrase "XXXX" -o "${l_backup_filer}${BACKUP_GPG_SUFFIX}" ; then
+			log_systemd "gpg2 $l_backup_file successfull."
+			l_ret_code=0
+		fi
+	fi
+
+	return "$l_ret_code"
+	
 }
 
 function targz_backup()
 {
-	rc_code=1
-	latest_backup="$(get_latest_rsync_backup)"
-	log_systemd "Found new backup $latest_backup ."
-	if [ "$latest_backup" != "-1" ] && [ "$latest_backup" != "" ] ; then
-		read timestamp < "$latest_backup""$TIMESTAMP_FILE"
-		targz_file="${BACKUP_TARGZ_DIR}${BACKUP_TARGZ_PREFIX}${timestamp}${BACKUP_TARGZ_SUFFIX}"
-		if tar -zcf "$targz_file" "$latest_backup"; then
-			log_systemd "Created $targz_file ."
-			rc_code=0
+	l_ret_code=1
+	l_latest_backup="$(get_latest_rsync_backup)"
+	log_systemd "Found new backup $l_latest_backup ."
+	if [ "$l_latest_backup" != "-1" ] && [ "$l_latest_backup" != "" ] ; then
+		read l_timestamp < "$l_latest_backup""$TIMESTAMP_FILE"
+		l_targz_file="${BACKUP_TARGZ_DIR}${BACKUP_TARGZ_PREFIX}${l_timestamp}${BACKUP_TARGZ_SUFFIX}"
+		if tar -zcf "$l_targz_file" "$l_latest_backup"; then
+			log_systemd "Created $l_targz_file ."
+			l_ret_code=0
 		fi
 	fi
 
-	return "$rc_code"
+	return "$l_ret_code"
 }
 
 function scp_backup()
 {
-	rc_code=1
-	mapfile -t backup_list < <(ls -dqtr "$BACKUP_TARGZ_DIR"* 2>/dev/null)
-	if [ "${#backup_list[@]}" -ge 1 ]; then
-		backup_file="${backup_list[0]}"
-		if scp "$backup_file" "$SSH_USER"@"$SSH_USER":"$REMOTE_BACKUP_PATH"; then
-			log_systemd "scp $backup_file successfull."
-			rc_code=0
+	l_ret_code=1
+	mapfile -t l_backup_list < <(ls -dqtr "$BACKUP_TARGZ_DIR"* 2>/dev/null)
+	if [ "${#l_backup_list[@]}" -ge 1 ]; then
+		l_backup_file="${l_backup_list[0]}"
+		if scp "$l_backup_file" "$SSH_USER"@"$SSH_USER":"$REMOTE_BACKUP_PATH"; then
+			log_systemd "scp $l_backup_file successfull."
+			l_ret_code=0
 		fi
 	fi
 
-	return "$rc_code"
+	return "$l_ret_code"
 }
 
 function log_systemd()
@@ -106,11 +194,30 @@ m_exit=1
 
 log_systemd "Starting Monthly backup."
 create_rsync_backup
-rc=$?
-if [ "$rc" -eq 0 ]; then
-	if targz_and_scp_backup; then
+ret_code=$?
+if [ "$ret_code" -eq 0 ]; then
+	m_exit=0
+fi
+
+if [ "$m_exit" -eq 0 ]; then
+	if targz_backup; then
 		m_exit=0
-	fi
+	else
+		m_exit=1
+fi
+
+if [ "$m_exit" -eq 0 ]; then
+	if targz_backup && rotate_targz; then
+		m_exit=0
+	else
+		m_exit=1
+fi
+
+if [ "$m_exit" -eq 0 ]; then
+	if gpg2_encrypt && scp_backup; then
+		m_exit=0
+	else
+		m_exit=1
 fi
 
 exit "$m_exit"
